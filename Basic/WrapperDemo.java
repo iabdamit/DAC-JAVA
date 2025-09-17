13d7d9a9c2ab51591f6e667dc9b8b136999d4ec2
ABD <abdamitbhandari@gmail.com>
2025-09-17 23:30:44 +0530

    wrapper class autoboxing ,autounboxing

--- Basic/WrapperDemo.java
+++ Basic/WrapperDemo.java
@@ -6,8 +6,13 @@ public class WrapperDemo {
 
         Integer num1 = num; // ! auto boxing
         System.out.println(num1);
-        // int num2 = num1.intValue(); // unboxing
+        // int num2 = num1.intValue(); // manual unboxing
         int num2 = num1; // ! auto unboxing
         System.out.println(num2);
+
+        String str = "284";
+        int num3 = Integer.parseInt(str);
+        System.out.println(num3);
+
     }
 }
