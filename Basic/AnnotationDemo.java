351d24209eb6fb25fa36d304698a46efb7591785
ABD <abdamitbhandari@gmail.com>
2025-09-21 19:00:22 +0530

    Annotations

--- Basic/AnnotationDemo.java
+++ Basic/AnnotationDemo.java
@@ -1,13 +1,19 @@
+//! Annotations are indeed used to give extra information to the compiler, tools, or frameworks about how a piece of code should be handled.
+
 class AnnotationA {
     public void show() {
-        System.out.println("in AnnotationA show ");
+        System.out.println("Inside Annotation A");
     }
 }
 
 class AnnotationB extends AnnotationA {
+
+    // ! @Override give information to the compiler that this method is overriding
+    // ! than comiler help us we donot do any mistake in overriding
+
     @Override
     public void show() {
-        System.out.println("in AnnotationB show ");
+        System.out.println("Inside Annotation B");
     }
 }
 
