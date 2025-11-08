98a9ae6875b0481f04e6f1ab3a8a51a046db8278
ABD <abdamitbhandari@gmail.com>
2025-11-09 01:13:56 +0530

    threads

--- Basic/Test.java
+++ Basic/Test.java
@@ -1,10 +1,26 @@
+import java.util.*;
+
 public class Test {
     public static void main(String[] args) {
-        String str = "Amit";
-        if (str == "Amit") {
-            System.out.println("yes");
-        } else {
-            System.out.println("No");
+        List<Integer> numList = new ArrayList<>();
+        List<Integer> numList2 = new ArrayList<>();
+        numList.add(20);
+        numList.add(25);
+        numList.add(40);
+        numList.set(1, 10);
+        numList2.add(25);
+        numList2.add(numList.get(0) + 5);
+        numList2.addAll(0, numList);
+        Iterator<Integer> iter = numList2.iterator();
+        int count = 0;
+        while (iter.hasNext()) {
+            if (numList2.contains(25)) {
+                count += 1;
+                numList2.set(0, count);
+            } else {
+                numList2.add(count);
+            }
+            System.out.println(iter.next());
         }
     }
 
--- Basic/ThreadDemo.java
+++ Basic/ThreadDemo.java
@@ -1,10 +1,13 @@
+//todo:  Multiple threads
+
 class TA extends Thread {
     public void run() {
-        for (int i = 0; i < 100; i++) {
+
+        for (int i = 1; i <= 100; i++) {
             System.out.println("hi");
             try {
                 Thread.sleep(1);
-            } catch (InterruptedException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -13,11 +16,11 @@ class TA extends Thread {
 
 class TB extends Thread {
     public void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.println("hello");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Hello");
             try {
                 Thread.sleep(1);
-            } catch (InterruptedException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -26,10 +29,13 @@ class TB extends Thread {
 
 public class ThreadDemo {
     public static void main(String[] args) {
-        TA obj = new TA();
-        TB obj1 = new TB();
-        System.out.println(obj.getPriority());
-        obj.start();
+        TA obj1 = new TA();
+        TB obj2 = new TB();
+
+        // todo Priority goes from 1 to 10
+        obj2.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(obj1.getPriority());
         obj1.start();
+        obj2.start();
     }
 }
