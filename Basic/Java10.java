2d17b30987c00669200974b9d02ae7339153f045
ABD <abdamitbhandari@gmail.com>
2025-11-19 00:06:28 +0530

    var in java

---
+++ Basic/Java10.java
@@ -0,0 +1,21 @@
+
+class Alien {
+
+}
+
+public class Java10 {
+    public static void main(String[] args) {
+
+        // ! var is only used for instance variable
+        int a = 5;
+        var b = 7;
+        int c;
+        // ! var d; we need to initilize it.
+        System.out.println(a + b);
+        int arr[] = new int[10];
+        var arr1 = new int[10];
+
+        Alien obj = new Alien();
+        var obj1 = new Alien();
+    }
+}
