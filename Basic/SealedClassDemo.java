32e20f859ee755254f871594a1cfa3372d45bd3e
ABD <abdamitbhandari@gmail.com>
2025-11-19 02:28:22 +0530

    Sealed class

--- Basic/SealedClassDemo.java
+++ Basic/SealedClassDemo.java
@@ -1,3 +1,30 @@
-public class SealedClassDemo {
+sealed class SCA permits SCB, SCC {
 
+}
+
+non-sealed class SCB extends SCA {
+
+}
+
+final class SCC extends SCA {
+
+}
+
+class SCD extends SCB {
+
+}
+
+sealed interface ad permits af {
+
+}
+
+non-sealed interface af
+        extends ad {
+
+}
+
+public class SealedClassDemo {
+    public static void main(String[] args) {
+
+    }
 }
